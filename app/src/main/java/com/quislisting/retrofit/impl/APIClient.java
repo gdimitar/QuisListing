fd95258b880c2ba428fad703b282e1230b06f94e
Dimitar Gavrilov <d.gavrilov@axeltra.com>
2018-05-14 14:54:50 +0200

    Modify retrofit default timeouts

--- app/src/main/java/com/quislisting/retrofit/impl/APIClient.java
+++ app/src/main/java/com/quislisting/retrofit/impl/APIClient.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.quislisting.rest.endpoints.RestRouter;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -14,10 +16,11 @@ public class APIClient {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
-
         final HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        final OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+        final OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor)
+                .connectTimeout(5, TimeUnit.SECONDS).readTimeout(5, TimeUnit.SECONDS)
+                .writeTimeout(5, TimeUnit.SECONDS).build();
 
         final Gson gson = new GsonBuilder().setLenient().create();
 
